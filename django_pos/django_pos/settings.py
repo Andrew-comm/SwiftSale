8698ff6ba7e36440c5d238070954ad6d250ab330
andrea <ndrwrono2001@gmail.com>
2024-05-06 11:31:00 +0300

    updates

--- django_pos/django_pos/settings.py
+++ django_pos/django_pos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-shfzstshi=+%g-@_=nc4_x=9xdpt1thdo=^k#($km%htb-se)(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','https://swiftsale.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','swiftsale.onrender.com']
 
 
 # Application definition
